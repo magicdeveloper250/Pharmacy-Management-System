12d4774f6006ed5a1461db21b661bd2dde6be72f
IMPANO <impanomanzienock@gmail.com>
2024-12-05 09:54:40 +0200

    addin db file

--- pharmacy_ms/settings.py
+++ pharmacy_ms/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = "pharmacy_ms.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "pharmacy_ms",
-        "USER": "root",
-        "PASSWORD": "root",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
