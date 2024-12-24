a281cd8837f2204d56e52f5aab754bac87b02b76
IMPANO <impanomanzienock@gmail.com>
2024-12-24 10:38:05 +0200

    adding database

--- pharmacy_ms/settings.py
+++ pharmacy_ms/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 'ENGINE': 'django.db.backends.postgresql',
 'NAME': 'pharmacy_ms',
 'USER': 'pharmacy_ms_user',
-'PASSWORD': '1XVxUSy8Ju7P6C2cHX9ghdX0IkUAlzhN@',
+'PASSWORD': '1XVxUSy8Ju7P6C2cHX9ghdX0IkUAlzhN',
 'HOST': 'dpg-ctl64v3v2p9s738dk7lg-a',
 'PORT': '5432',  
 }
