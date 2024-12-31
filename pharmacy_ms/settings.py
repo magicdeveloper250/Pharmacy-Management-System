0e30501bd185f52de66c72bdd9ff9cc4a09dcb1e
IMPANO <impanomanzienock@gmail.com>
2024-12-31 09:48:44 +0200

    adding invoice print functionality

--- pharmacy_ms/settings.py
+++ pharmacy_ms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages_constant
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +83,9 @@ AUTHENTICATION_BACKENDS = [
 
 WSGI_APPLICATION = "pharmacy_ms.wsgi.application"
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-
+MESSAGE_TAGS = {
+    messages_constant.ERROR: 'danger',
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
